925c0ced03c9fcefde69459b218417899d2b6f29
YDZ <ydz627@gmail.com>
2017-07-08 17:25:46 +0800

    1. 增加 layout 布局 2. 增加 navigationBar

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-import foo from './src/foo.vue';
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
+import Layout from './components/layout';
 import * as uiv from 'uiv';
+// import cordova from './mobile-index'  // 打开此行代码的注释可以打 cordova 的包
 
-foo.el = '#root'
-export default new Vue(foo);
+Vue.use(uiv);
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
+
+// cordova.onDeviceReady = () => {  // 打开此行代码的注释可以打 cordova 的包
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<Layout/>',
+  components: {
+    Layout
+  }
+  //  render: h => h(App)
+});
+// }
+
+// cordova.initialize() // 打开此行代码的注释可以打 cordova 的包
---
+++ src/components/layout.vue
@@ -0,0 +1,41 @@
+<template>
+<div id="__wrapper">
+
+  <navgation-bar>
+  </navgation-bar>
+
+  <div class="app-content">
+    <keep-alive>
+      <transition name="drop">
+        <router-view></router-view>
+      </transition>
+    </keep-alive>
+  </div>
+
+  <footerView>
+  </footerView>
+
+</div>
+</template>
+
+<script>
+import navgationBar from './navigationBar';
+import bookCard from './bookCard';
+import footerView from './footerView';
+
+export default {
+  components: {
+    navgationBar,
+    bookCard,
+    footerView
+  },
+  created() {
+    this.$store.dispatch('loadUser');
+  }
+};
+</script>
+
+<style>
+@import '../style/style.css';
+@import '../style/bootstrap-flat.min.css';
+</style>
